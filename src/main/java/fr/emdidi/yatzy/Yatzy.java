51d2bd3c312a37a2c96ecfd61ca788bfca97a501
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2021-02-12 15:37:27 +0100

    [TS] Refactor smallStraight method

--- src/main/java/fr/emdidi/yatzy/Yatzy.java
+++ src/main/java/fr/emdidi/yatzy/Yatzy.java
@@ -115,20 +115,9 @@ public class Yatzy {
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[0] == 1 &&
-            tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1)
-            return 15;
-        return 0;
+        List<Integer> dices = Arrays.asList(d1, d2, d3, d4, d5).stream().sorted().collect(Collectors.toList());
+        boolean result =  Arrays.asList(1, 2, 3, 4, 5).equals(dices);
+        return result ? 15 : 0;
     }
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
