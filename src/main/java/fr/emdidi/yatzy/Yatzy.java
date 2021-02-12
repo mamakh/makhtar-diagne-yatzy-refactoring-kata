d189ccd3dd311bd48c6219469b67ba12ba28b39b
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2021-02-12 15:27:33 +0100

    [TS] Refactor four_of_a_kind method

--- src/main/java/fr/emdidi/yatzy/Yatzy.java
+++ src/main/java/fr/emdidi/yatzy/Yatzy.java
@@ -103,19 +103,9 @@ public class Yatzy {
         return IntStream.range(0, 2).reduce(0, (a, b) -> a + pairsFound.get(b) * 2);
     }
 
-    public static int four_of_a_kind(int _1, int _2, int d3, int d4, int d5)
+    public static int four_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[_1-1]++;
-        tallies[_2-1]++;
-        tallies[d3-1]++;
-        tallies[d4-1]++;
-        tallies[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (tallies[i] >= 4)
-                return (i+1) * 4;
-        return 0;
+        return highestOfAKind(4, d1, d2, d3, d4, d5)  * 4;
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
