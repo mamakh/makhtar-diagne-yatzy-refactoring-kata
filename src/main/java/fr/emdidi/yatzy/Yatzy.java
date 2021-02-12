4988ee588f374aa2a9f3dfd2e13b4dc5f1ad87a2
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2021-02-12 13:30:56 +0100

    [DS] Refactor ones method

--- src/main/java/fr/emdidi/yatzy/Yatzy.java
+++ src/main/java/fr/emdidi/yatzy/Yatzy.java
@@ -23,15 +23,7 @@ public class Yatzy {
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+        return IntStream.of(d1, d2, d3, d4, d5).filter(dice -> dice == 1).reduce(0, (a,b) -> a + 1);
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
