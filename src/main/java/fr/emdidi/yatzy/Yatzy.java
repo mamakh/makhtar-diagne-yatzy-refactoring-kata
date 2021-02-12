fe2a932428a37a85a2343172b6ffa2b8b4819d40
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2021-02-12 13:53:03 +0100

    [TS] Refactor sixes method

--- src/main/java/fr/emdidi/yatzy/Yatzy.java
+++ src/main/java/fr/emdidi/yatzy/Yatzy.java
@@ -64,11 +64,7 @@ public class Yatzy {
 
     public int sixes()
     {
-        int sum = 0;
-        for (int at = 0; at < dice.length; at++) 
-            if (dice[at] == 6)
-                sum = sum + 6;
-        return sum;
+        return accumulator(6, dice);
     }
 
     public static int score_pair(int d1, int d2, int d3, int d4, int d5)
