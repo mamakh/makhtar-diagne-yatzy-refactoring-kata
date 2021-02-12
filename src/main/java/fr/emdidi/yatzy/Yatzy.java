7165cac1b01fe921d9af5435fd3525da6e4fa57a
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2021-02-12 15:42:31 +0100

    [TS] Refactor largeStraight method

--- src/main/java/fr/emdidi/yatzy/Yatzy.java
+++ src/main/java/fr/emdidi/yatzy/Yatzy.java
@@ -122,20 +122,9 @@ public class Yatzy {
 
     public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
+        List<Integer> dices = Arrays.asList(d1, d2, d3, d4, d5).stream().sorted().collect(Collectors.toList());
+        boolean result =  Arrays.asList(2, 3, 4, 5, 6).equals(dices);
+        return result ? 20 : 0;
     }
 
     public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
