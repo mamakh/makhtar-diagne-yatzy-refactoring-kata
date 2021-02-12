fac5a3ca6d148116822b338b314ec63c02420425
Makhtar DIAGNE <makhtar.diagne@teamdlab.com>
2021-02-12 15:28:39 +0100

    [TS] Refactor three_of_a_kind method

--- src/main/java/fr/emdidi/yatzy/Yatzy.java
+++ src/main/java/fr/emdidi/yatzy/Yatzy.java
@@ -110,17 +110,7 @@ public class Yatzy {
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5)
     {
-        int[] t;
-        t = new int[6];
-        t[d1-1]++;
-        t[d2-1]++;
-        t[d3-1]++;
-        t[d4-1]++;
-        t[d5-1]++;
-        for (int i = 0; i < 6; i++)
-            if (t[i] >= 3)
-                return (i+1) * 3;
-        return 0;
+        return highestOfAKind(3, d1, d2, d3, d4, d5)  * 3;
     }
 
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5)
